bcdfa076b6e23e99ea63a21419a323f3045281b4
poormonitor <poormonitor@outlook.com>
2021-02-06 14:40:59 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Xiaomi Router SSH Enabler
+
+适用于**AX3600/AX1800/AX5/AC2100**，方法来自[恩山论坛](https://www.right.com.cn/forum/thread-4032490-1-1.html)。
+
+基于Python和PyQT5开发。
+
+## Screenshot
+
+![image-20210206143545477](https://cdn.jsdelivr.net/gh/poormonitor/image@master/20210206/823b06c9be5d6c5b6e13f7cf91029548.png)
+
+## Usage
+
+在Release中下载构建好的exe文件并运行，填写stok和希望设定的passwd并按ok执行。看到success提示即为成功。
+
+**Attention:	**stok错误也会返回success；ip错误超时程序可能无响应。
+
---
+++ main.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/6 13:17
+# @Author  : Johnson Sun (@poormonitor)
+# @Email   : poormonitor@outlook.com
+# @File    : main.py
+
+import sys
+import requests
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(375, 235)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(130, 180, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(80, 50, 21, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(70, 90, 31, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(40, 130, 72, 15))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(120, 50, 121, 21))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(120, 90, 191, 21))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_3.setGeometry(QtCore.QRect(120, 130, 191, 21))
+        self.lineEdit_3.setText("")
+        self.lineEdit_3.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+
+        self.retranslateUi(Dialog)
+        self.pushButton.clicked.connect(Dialog.act)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Xiaomi Router SSH Enabler"))
+        self.pushButton.setText(_translate("Dialog", "OK"))
+        self.label.setText(_translate("Dialog", "IP"))
+        self.label_2.setText(_translate("Dialog", "stok"))
+        self.label_3.setText(_translate("Dialog", "Password"))
+        self.lineEdit.setText(_translate("Dialog", "192.168.31.1"))
+
+
+class MainDialog(QDialog):
+    def __init__(self, parent=None):
+        super(QDialog, self).__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+    def act(self):
+        stok = self.ui.lineEdit_2.text()
+        passwd = self.ui.lineEdit_3.text()
+        ip = self.ui.lineEdit.text()
+        result1 = requests.get(
+            "http://"+ip+"/cgi-bin/luci/;stok="+stok+"/api/misystem/set_config_iotdev?bssid=Xiaomi&user_id=longdike&ssid=-h%3B%20nvram%20set%20ssh_en%3D1%3B%20nvram%20commit%3B%20sed%20-i%20's%2Fchannel%3D.*%2Fchannel%3D%5C%22debug%5C%22%2Fg'%20%2Fetc%2Finit.d%2Fdropbear%3B%20%2Fetc%2Finit.d%2Fdropbear%20start%3B")
+        result2 = requests.get(
+            "http://"+ip+"/cgi-bin/luci/;stok="+stok+"/api/misystem/set_config_iotdev?bssid=Xiaomi&user_id=longdike&ssid=-h%ds3B%20echo%20-e%20'"+passwd+"%5Cn"+passwd+"'%20%7C%20passwd%20root%3B")
+        if result1.status_code == 200 and result2.status_code == 200:
+            QMessageBox.information(self, 'success', 'success')
+        else:
+            QMessageBox.warning(self, 'error', 'something went wrong')
+
+if __name__ == '__main__':
+    myapp = QApplication(sys.argv)
+    myDlg = MainDialog()
+    myDlg.show()
+    sys.exit(myapp.exec_())
